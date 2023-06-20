c46b3b1f127ab6d183ed445889d6ed7ce1c26dc7
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-06-20 15:34:59 -0700

    insert_stack.c

---
+++ insert_stack.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+/**
+ * f_pnsrt - node to the stack be added
+ * @head: stack pile head
+ * @counter: line_number
+ * Return: no 0 return
+*/
+void f_pnsrt(stack_t **head, unsigned int counter)
+{
+int n, y = 0, fleag = 0;
+
+if (bur.arg)
+{
+if (bur.arg[0] == '-')
+y++;
+for (; bur.arg[y] != '\0'; y++)
+{
+if (bur.arg[y] > 57 || bur.arg[y] < 48)
+fleag = 1; }
+if (fleag == 1)
+{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+fclose(bur.fiie);
+free(bur.conttrnt);
+free_stack(*head);
+exit(EXIT_FAILURE); }}
+else
+{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+fclose(bur.fiie);
+free(bur.conttrnt);
+free_stack(*head);
+exit(EXIT_FAILURE); }
+n = atoi(bur.arg);
+if (bur.lffiy == 0)
+addelode(head, n);
+else
+addqueue(head, n);
+}
+
